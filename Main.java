be4da651ca0a97f7e0f44d1dbb25da169ce5afaf
Mohammad Abdul Quddus Salam <quddussalam555@gmail.com>
2025-10-23 08:55:11 +0700

    Initial commit, added the outlines for game's input and logic.
    Co-authored-by: Vluuuu <Vluuuu@users.noreply.github.com>
    Co-authored-by: rezaabdullatif50-prog <rezaabdullatif50-prog@users.noreply.github.com>

---
+++ Main.java
@@ -0,0 +1,113 @@
+/*
+ * Game overview: 
+ * Permainan Pingdol (Tic Tac Toe).
+ * Pemain mulai dari X.
+ * Saat mulai, user 1 dan 2 memasukan namanya.
+ *
+ * Game logic:
+ * User menggunakan WASD untuk memilih baris dan kolom.
+ * Untuk setiap langkah, program menentukan apakah ada yang selesai (vertikal, horizontal, dan diagonal).
+ * Jika ditemukan pemenang, program selesai dan menampilkan nama pemenang.
+ * Jika seri, maka program mengulang kembali. 
+ */
+
+// Bagi tugas, Johan dan Dewo logika.
+// Quddus dan Latif inputnya.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("===================");
+        System.out.println("PROGRAM KOTAK AJAIB");
+        System.out.println("===================");
+    }
+
+    public static int[][] input() {
+        Scanner scan = new Scanner(System.in);
+        char inputChar = scan.nextLine().toLowerCase().charAt(0);
+        int baris = 0;
+        int kolom = 0;
+        while (true) {
+            System.out.printf("Pilih kotak (baris %d | kolom %d)", baris, kolom);
+            if (inputChar != 'a' && inputChar != 'w' && inputChar != 's' && inputChar != 'd') {
+                System.out.println("Gunakan WASD untuk memilih kotak!");
+                continue;
+            }
+            switch (inputChar) {
+                case 'w':
+                    System.out.println();
+                    break;
+                case 'a':
+                    System.out.println();
+                    break;
+                case 's':
+                    System.out.println();
+                    break;
+                case 'd':
+                    System.out.println();
+                    break;
+                    continue;
+            }
+        }
+    }
+
+    public static void logic(String[] args) {
+        char[][] matrix;
+        matrix = new char[3][3];
+
+        // dewo
+        if (matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2]) {
+            if (matrix[0][0] == 'x') {
+                System.out.println("Selamat player 1 menang");
+            } else {
+                System.out.println("selamat player 2 menang");
+            }
+        } else if (matrix[0][2] == matrix[1][1] && matrix[1][1] == matrix[2][0]) {
+            if (matrix[0][2] == 'x') {
+                System.out.println("Selamat player 1 menang");
+            } else {
+                System.out.println("selamat player 2 menang");
+            }
+        } else if (matrix[1][0] == matrix[1][1] && matrix[1][2] == matrix[2][0]) {
+            if (matrix[1][2] == 'x') {
+                System.out.println("Selamat player 1 menang");
+            } else {
+                System.out.println("selamat player 2 menang");
+            }
+        } else if (matrix[2][0] == matrix[2][1] && matrix[2][1] == matrix[2][2]) {
+            if (matrix[2][2] == 'x') {
+                System.out.println("Selamat player 1 menang");
+            } else {
+                System.out.println("selamat player 2 menang");
+            }
+
+        }
+        // johan
+        else if (matrix[0][0] == matrix[1][0] && matrix[1][0] == matrix[2][0]) {
+            if (matrix[0][0] == 'x') {
+                System.out.println("Selamat player 1 menang");
+            } else {
+                System.out.println("selamat player 2 menang");
+            }
+        } else if (matrix[0][1] == matrix[1][1] && matrix[1][1] == matrix[2][1]) {
+            if (matrix[0][1] == 'x') {
+                System.out.println("Selamat player 1 menang");
+            } else {
+                System.out.println("selamat player 2 menang");
+            }
+        } else if (matrix[0][2] == matrix[1][2] && matrix[1][2] == matrix[2][2]) {
+            if (matrix[0][2] == 'x') {
+                System.out.println("Selamat player 1 menang");
+            } else {
+                System.out.println("selamat player 2 menang");
+            }
+        } else if (matrix[0][0] == matrix[0][1] && matrix[0][1] == matrix[0][2]) {
+            if (matrix[0][0] == 'x') {
+                System.out.println("Selamat player 1 menang");
+            } else {
+                System.out.println("selamat player 2 menang");
+            }
+        }
+    }
+}
